6d01f2638e2af5638ab07f50156620a0fa3619c9
zer0silver <duddms0_0@naver.com>
2022-02-22 13:22:48 +0900

    반복문

---
+++ javascriptBasic/prac05.js
@@ -0,0 +1,14 @@
+let cars = ["sonata","bmw"]; 
+
+for(car of cars) {
+    console.log(car);
+}
+
+for(let i = 0; i<cars.length; i++){
+    let car = cars[i]
+    console.log(car);
+}
+
+cars.map((car)=>{
+    console.log(car);
+});
