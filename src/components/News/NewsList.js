a3a239e3cd78e4a92609561f960237a9a06edab9
zer0silver <78026977+zer0silver@users.noreply.github.com>
2022-02-23 22:51:46 +0900

    news url 추가

--- src/components/News/NewsList.js
+++ src/components/News/NewsList.js
@@ -5,9 +5,12 @@ const NewsList = ({searchResultList}) => {
     {
       // react-jsx-dev-runtime.development.js:117 Warning: Each child in a list should have a unique "key" prop.
       // 에러 해결 하기 -> key를 추가해준다
-      searchResultList.map(({title}, index)=>{ // 두번째 인자는 인덱스 이다. 콜백함수?
+      searchResultList.map(({title,url}, index)=>{ // 두번째 인자는 인덱스 이다. 콜백함수?
         return(
-          <p key={index}>{title}</p>
+          <>
+            <a href={url} key={index}>{title}</a>
+            <br></br>
+          </>
         )
       })
     }
