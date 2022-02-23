fe31ca038865efa4333cdc9040b8e8276644f5b2
zer0silver <78026977+zer0silver@users.noreply.github.com>
2022-02-23 22:47:04 +0900

    key 관련 에러 해결 - 콜백함수?

--- src/components/News/NewsList.js
+++ src/components/News/NewsList.js
@@ -3,9 +3,11 @@ import React from 'react';
 const NewsList = ({searchResultList}) => {
   return <div>
     {
-      searchResultList.map((news)=>{
+      // react-jsx-dev-runtime.development.js:117 Warning: Each child in a list should have a unique "key" prop.
+      // 에러 해결 하기 -> key를 추가해준다
+      searchResultList.map(({title}, index)=>{ // 두번째 인자는 인덱스 이다. 콜백함수?
         return(
-          <p>{news.title}</p>
+          <p key={index}>{title}</p>
         )
       })
     }
