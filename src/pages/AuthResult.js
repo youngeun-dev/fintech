fb57d6314a200a7ec90e322be5d74c6243318123
zer0silver <78026977+zer0silver@users.noreply.github.com>
2022-02-24 11:15:21 +0900

    queryString으로 인증코드 가져오기

--- src/pages/AuthResult.js
+++ src/pages/AuthResult.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import HeaderWhite from '../components/HeaderWhite';
+import { useLocation } from 'react-router-dom';
+import queryString from "query-string";
 
 const AuthResult = () => {
+    console.log(useLocation().search);
+    const parsedResult = queryString.parse(useLocation().search);
+    console.log(parsedResult.code);
   return (
-    <div>AuthResult</div>
-  )
-}
+    <div>
+        <HeaderWhite title={"AccessToken 요청"}></HeaderWhite>
+        <p>인증코드 : {parsedResult.code}</p>
+    </div>
+    
+  );
+};
 
-export default AuthResult
+export default AuthResult;
