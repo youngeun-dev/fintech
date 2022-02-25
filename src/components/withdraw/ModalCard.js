2d5cbe81c28fe47fc379310bdcc8c9146acd02d5
zer0silver <78026977+zer0silver@users.noreply.github.com>
2022-02-25 15:28:36 +0900

    출금 이체 요청 완료

--- src/components/withdraw/ModalCard.js
+++ src/components/withdraw/ModalCard.js
@@ -27,7 +27,7 @@ const WithDrawButton = styled.button`
 `;
 
 const ModalCard = ({ bankName, fintechUseNo, tofintechno }) => {
-  // input data 받아온다음에 결제 버튼을 눌렀을때 axios 출금 이체를 바생시키기;
+  // input data 받아온다음에 결제 버튼을 눌렀을때 axios 출금 이체를 발생시키기;
   const [amount, setamount] = useState("");
   const handleAmountChange = (e) => {
     const { value } = e.target;
@@ -35,12 +35,46 @@ const ModalCard = ({ bankName, fintechUseNo, tofintechno }) => {
   };
   const genTransId = () => {
     let countnum = Math.floor(Math.random() * 1000000000) + 1;
-    let transId = "M202200381" + countnum; //이용기과번호 본인것 입력
+    let transId = "M202200381U" + countnum; //이용기관번호 본인것 입력
     return transId;
   };
 
   const handleWithdraw = () => {
     //axios call
+     //axios call
+    //출금 이체 API 요청 작성하기
+    let sendData = JSON.stringify({
+      bank_tran_id: genTransId(),
+      cntr_account_type: "N",
+      cntr_account_num: "100000000001",
+      dps_print_content: "이용료",
+      fintech_use_num: fintechUseNo,
+      wd_print_content: "이용료",
+      tran_amt: amount,
+      tran_dtime: "20220910101921",
+      req_client_name: "홍길동",
+      req_client_num: "H1234",
+      req_client_fintech_use_num: fintechUseNo,
+      transfer_purpose: "ST",
+      recv_client_name: "홍길동",
+      recv_client_bank_code: "097",
+      recv_client_account_num: "100000000001",
+    });
+
+    const option = {
+      method: "POST",
+      url: "/v2.0/transfer/withdraw/fin_num",
+      headers: {
+        Authorization: `bearer ${localStorage.getItem("accessToken")}`,
+      },
+      data: sendData,
+    };
+
+
+        axios(option).then(({ data }) => {
+            console.log(data);
+            //setamount(data.res_list);
+        });
   };
 
   const deposit = () => {};
